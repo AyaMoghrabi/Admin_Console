400cf5d64d35958b4b610620750079d1049a1377
AyaMoghrabi <a220094@students.gu.edu.lb>
2025-06-30 15:38:21 +0300

    updated project files

--- admin-console/src/Pages/Users.js
+++ admin-console/src/Pages/Users.js
@@ -1,35 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import "../App.css";
-
-// function Users() {
-//   const [users, setUsers] = useState([]);
-
-//   useEffect(() => {
-//     fetch('http://localhost:5000/api/users')
-//       .then(res => res.json())
-//       .then(data => setUsers(data));
-//   }, []);
-
-//   return (
-//     <div className="table-container">
-//       <h2>Users</h2>
-//       <table>
-//         <thead>
-//           <tr><th>ID</th><th>Name</th><th>Email</th></tr>
-//         </thead>
-//         <tbody>
-//           {users.map((user) => (
-//             <tr key={user.id}><td>{user.id}</td><td>{user.name}</td><td>{user.email}</td></tr>
-//           ))}
-//         </tbody>
-//       </table>
-//     </div>
-//   );
-// }
-
-// export default Users;
-
-
 import React, { useEffect, useState } from "react";
 import "../App.css";
 
